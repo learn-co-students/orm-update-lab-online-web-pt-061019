a124c1d3674dd0f1f9f298a9a62fded8a7e8a79f
David Foote <david.a.foote@gmail.com>
2019-08-21 20:08:09 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -34,13 +34,17 @@ class Student
       self.update
     else
       sql = <<-SQL
-        INSERT INTO students (
+        INSERT INTO students
           (name, grade)
           VALUES (?, ?)
-          )
-      SQL
-      DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+        SQL
+        DB[:conn].execute(sql, self.name, self.grade)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
+  end
+  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id =?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 end
