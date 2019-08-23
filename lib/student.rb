61ee43e91916a4ee25a9d019ac5b0bd1c6c77b5f
David Foote <david.a.foote@gmail.com>
2019-08-23 03:33:37 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -26,6 +26,14 @@ class Student
     DB[:conn].execute(sql)
   end
   
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
+  
   def save
     if self.id
       self.update
